cd6816974fe308d6edfc9b207ad2d237933595b5
AmirHossein_Razlighi <arazlighi@gmail.com>
2021-04-23 23:38:06 +0430

    ADD MODELS : CARDS (And SubClasses) , Decks

---
+++ Spell.java
@@ -0,0 +1,14 @@
+public class Spell extends Card {
+
+    public Spell(String name, Type type) {
+        super(name, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.getName() +
+                "\nSpell" +
+                "\nType: " + this.getType() +
+                "\nDescription: " + this.getDescription();
+    }
+}
---
+++ Trap.java
@@ -0,0 +1,14 @@
+public class Trap extends Card {
+
+    public Trap(String name, Type type) {
+        super(name, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.getName() +
+                "\nTrap" +
+                "\nType: " + this.getType() +
+                "\nDescription: " + this.getDescription();
+    }
+}
